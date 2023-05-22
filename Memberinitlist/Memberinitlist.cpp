50662fd0c2e5befe345869afece6bbc70ed0df1c
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-05-22 14:58:43 +0700

    perbaikan terakhir

--- Memberinitlist/Memberinitlist.cpp
+++ Memberinitlist/Memberinitlist.cpp
@@ -19,6 +19,27 @@ public:
 		return a + b;
 	}
 }; 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama):
+	nama(pNama) {
+		cout << " orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << " orang dihapus\n" << endl;
+	}
+
+	int jumlah(int a, int b) {
+		return a + b;
+	}
+}; 
 
 class pelajar:public orang {
 public:
@@ -41,7 +62,31 @@ public:
 int main() {
 	pelajar siswa1(" Andi Laksono", " SMAN 1 Bantul");
 	cout << siswa1.perkenalan();
-	cout << " Hasil = " << siswa1.jumlah(10.90) << endl;
+	cout << " Hasil = " << siswa1.jumlah(10,90) << endl;
+
+	return 0;
+}class pelajar:public orang {
+public:
+	string sekolah;
+	
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "Pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << " Pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return " Hallo nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+
+	}
+};
+
+int main() {
+	pelajar siswa1(" Andi Laksono", " SMAN 1 Bantul");
+	cout << siswa1.perkenalan();
+	cout << " Hasil = " << siswa1.jumlah(10,90) << endl;
 
 	return 0;
 }
